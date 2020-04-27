1dbeebf2fe76bd70036323b901d5292399b9a6e8
Zackary Flake <flakez82@students.rowan.edu>
2020-04-27 12:59:26 -0400

    JSON can now be pulled from site and parsed.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from apiClients import album
+from apiClients import album, api_common
+import requests
+import json
 
 print("Enter album: ")
 alb = input()
 print("Enter artist: ")
 artist = input()
 
-query = album.get_info(artist, alb)
-print(query)
+query = "http://ws.audioscrobbler.com/2.0/?"
+query += album.get_info(artist, alb)
+query += "&api_key=" + api_common.get_api_key()
+query += "&format=json"
+
+response = requests.post(query)
+info = json.loads(response.text)
+
+print(info['album']['name'])
