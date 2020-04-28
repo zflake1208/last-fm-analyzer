b76a882fcca3e88c5bcee63eff371138d2fd22cc
Zack Flake <zflake1208@gmail.com>
2020-04-28 15:03:50 -0400

    Graph style adjustments.

--- graph.py
+++ graph.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 
 def create_graph(x_label, y_label, x_values, y_values):
+    label_font = {'size': 12}
+    fig, ax = plt.subplots()
+    fig.set_size_inches(20, 10)
+    ax.ticklabel_format(style='plain')
     plt.bar(x_values, y_values)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
+    plt.xticks(fontsize=8)
+    plt.yticks(fontsize=8)
+    plt.xlabel(x_label, label_font)
+    plt.ylabel(y_label, label_font)
     plt.show()
